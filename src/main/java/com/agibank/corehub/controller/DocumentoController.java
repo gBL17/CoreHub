272aa8ee1e4edbee5c31fd3ce871d1ea9691033c
heitor.delvecchi <heitordelveccho@hotmail.com>
2025-04-04 09:08:46 -0300

    :tada: Criação das classes referente a Documento

---
+++ src/main/java/com/agibank/corehub/controller/DocumentoController.java
@@ -0,0 +1,60 @@
+package com.agibank.corehub.controller;
+
+import com.agibank.corehub.beans.Documento;
+import com.agibank.corehub.dao.DocumentoDAO;
+
+import java.sql.SQLException;
+
+public class DocumentoController {
+    private DocumentoDAO documentoDAO = new DocumentoDAO();
+    int idUsuario = 0;
+    public DocumentoController() throws SQLException {
+    }
+
+    public int adicionarDocumento(int id_usuario, String tipo, String numero, String arquivo) throws SQLException {
+
+        try {
+            Documento documento = new Documento(1, id_usuario, tipo, numero, arquivo);
+            DocumentoDAO.criarDocumento(documento);
+            idUsuario = id_usuario;
+            inserirTipoDocumento(numero);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return 0;
+    }
+
+    public void deletarDocumento(int id_documento){
+        try{
+            DocumentoDAO.deletarDocumento(id_documento);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int alterarDocumento(int id_usuario, String tipo, String numero, String arquivo) throws SQLException {
+        DocumentoDAO.atualizarDocumento(id_usuario, tipo, numero, arquivo);
+        inserirTipoDocumento(numero);
+        return id_usuario;
+    }
+
+    public void inserirTipoDocumento(String numero) {
+        try {
+            if (numero.length() == 11) {
+                DocumentoDAO.determinarTipoDocumento("cpf", idUsuario);
+            } else if (numero.length() == 8) {
+                DocumentoDAO.determinarTipoDocumento("rg", idUsuario);
+            } else if (numero.length() == 9) {
+                DocumentoDAO.determinarTipoDocumento("cnh", idUsuario);
+            } else if (numero.length() == 14) {
+                DocumentoDAO.determinarTipoDocumento("cnpj", idUsuario);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void adicionarDocumento(String numero) {
+    }
+}
