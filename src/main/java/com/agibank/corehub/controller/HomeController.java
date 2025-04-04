831e34c8bf6685002777372c654f7881f4af5887
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-04 16:13:56 -0300

    :sparkles: feat(Listar contas de usuario) cria funcionalidade que exibeas contas do usuario na view

--- src/main/java/com/agibank/corehub/controller/HomeController.java
+++ src/main/java/com/agibank/corehub/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.agibank.corehub.controller;
 
-import com.agibank.corehub.beans.Usuario;
-import com.agibank.corehub.beans.conta.Conta;
 import com.agibank.corehub.controller.conta.ContaController;
-import com.agibank.corehub.dao.conta.ContaDAO;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -21,10 +18,60 @@ import javafx.stage.Stage;
 
 public class HomeController implements Initializable {
     private static final Usuario usuarioLogado = UsuarioLogadoController.getInstance().getUsuario();
+public class HomeController implements Initializable {
+    private int idUsuario;
+
+    public void setIdUsuario(int id){
+        this.idUsuario = id;
+        try {
+            carregarContasUsuario();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
 
     @FXML
     public Label labelNomeUsuario;
 
+    @FXML
+    private ListView<String> listViewContas;
+
+    private ObservableList<String> contasExibidas = FXCollections.observableArrayList();
+    private ArrayList<Conta> contasUsuario = new ArrayList<>();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            carregarContasUsuario();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void navegarConta(ActionEvent actionEvent) throws IOException {
+        int selectedIndex = listViewContas.getSelectionModel().getSelectedIndex();
+
+        if (selectedIndex >= 0) {
+            Conta contaSelecionada = contasUsuario.get(selectedIndex);
+
+            int idConta = 0;
+
+            try {
+                ContaDAO dao = new ContaDAO();
+                idConta = dao.buscarIdContaPorAgenciaENumero(
+                        contaSelecionada.getIdAgencia(), contaSelecionada.getNumero()
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/agibank/corehub/views/conta.fxml"));
+            Parent root = loader.load();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         labelNomeUsuario.setText(usuarioLogado.getNome());
@@ -36,11 +83,31 @@ public class HomeController implements Initializable {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/agibank/corehub/views/conta.fxml"));
         Parent root = loader.load();
 
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 412, 915);
-        stage.setScene(scene);
-        stage.show();
+            ContaController controller = loader.getController();
+            controller.setIdConta(idConta);
+
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 412, 915);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            System.out.println("Nenhuma conta selecionada.");
+        }
     }
+
+
+    private void carregarContasUsuario() throws Exception {
+        ContaDAO dao = new ContaDAO();
+        contasUsuario = dao.listarContasUsuario(idUsuario);
+
+        for (Conta conta : contasUsuario) {
+            contasExibidas.add("Conta: " + conta.getTipo());
+        }
+
+        listViewContas.setItems(contasExibidas);
+    }
+
+
 
     public Conta buscarConta(int id) throws SQLException {
         ContaDAO contaDAO = new ContaDAO();
