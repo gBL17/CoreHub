1800e8da4eee0a7d7ade8070a5b3d9ebab8a65a7
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-04 18:09:52 -0300

    :sparkles: feat(TelaInical) cria tela inicial para entrar na conta ou cadastrar uma nova

---
+++ src/main/java/com/agibank/corehub/controller/TelaInicialController.java
@@ -0,0 +1,27 @@
+package com.agibank.corehub.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TelaInicialController {
+
+    @FXML
+    private void handleLogin(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/agibank/corehub/views/home/login.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 412, 915);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
