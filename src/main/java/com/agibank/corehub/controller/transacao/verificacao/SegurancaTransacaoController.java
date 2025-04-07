fe0e93b8ecf64f3b3b562745c27d7874885679be
heitor.delvecchi <heitordelveccho@hotmail.com>
2025-04-07 10:43:33 -0300

    :sparkles: Atualização da Calsse SegurancaTransacaoController, novos métodos adicionados: verificarHorario, verificarMediaValor e verificarMaiorValor

--- src/main/java/com/agibank/corehub/controller/transacao/verificacao/SegurancaTransacaoController.java
+++ src/main/java/com/agibank/corehub/controller/transacao/verificacao/SegurancaTransacaoController.java
@@ -1,9 +1,50 @@
 package com.agibank.corehub.controller.transacao.verificacao;
 
 import com.agibank.corehub.beans.transacao.Transacao;
+import com.agibank.corehub.dao.VerificacaoSegurancaDAO;
+
+import java.sql.SQLException;
+import java.time.LocalTime;
 
 public class SegurancaTransacaoController {
-    public void verificarSegurancaTransacao(Transacao transacao){
+    public void verificarSegurancaTransacao(Transacao transacao) throws SQLException {
         //todo Adicionar metodos de segurança
+        VerificacaoSegurancaDAO verificacaoSegurancaDAO = new VerificacaoSegurancaDAO();
+        double media = (int) verificacaoSegurancaDAO.mediaValores(transacao.getIdContaOrigem());
+        double maior = (int) verificacaoSegurancaDAO.maiorValor(transacao.getIdContaOrigem());
+        double valor = transacao.getValor();
+
+        verificarHorario();
+        verificarMediaValor(valor, media);
+        verificarMaiorValor(valor, maior);
+
+    }
+
+    public boolean verificarHorario() throws  SQLException {
+        LocalTime horarioAtual = LocalTime.now();
+        LocalTime start = LocalTime.of(6, 0);  // 6:00 AM
+        LocalTime end = LocalTime.of(22, 0);   // 10:00 PM
+
+        if (!(horarioAtual.isAfter(start) && horarioAtual.isBefore(end))) {
+            System.out.println("Transação fora do horário permitido.");
+            return  false;
+        }
+        return true;
+    }
+
+    public boolean verificarMaiorValor(double valor ,double maiorValor) throws  SQLException {
+        if(valor > maiorValor) {
+            System.out.println("Transação excede o maior valor permitido.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean verificarMediaValor(double valor, double mediaValores) throws  SQLException {
+        if(valor > mediaValores * 1.5) {
+            System.out.println("Transação acima do limite baseado na média.");
+            return false;
+        }
+        return true;
     }
 }
