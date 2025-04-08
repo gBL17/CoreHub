a19b41dbe2e3f2c8634ddeeb749f7352e6f6169f
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-08 00:01:18 -0300

    :sparkles: feat(tipoTransacao.fxml) cria design para tela e ajusta metodo de acordo com a interface

--- src/main/java/com/agibank/corehub/controller/transacao/TipoTransacaoController.java
+++ src/main/java/com/agibank/corehub/controller/transacao/TipoTransacaoController.java
@@ -13,71 +13,63 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class TipoTransacaoController {
+
     private Transacao transacao = new Transacao();
 
     @FXML
-    private RadioButton radioPix;
+    private StackPane pixButton;
 
     @FXML
-    private RadioButton radioDoc;
+    private StackPane docButton;
 
     @FXML
-    private RadioButton radioTed;
+    private StackPane tedButton;
 
-    @FXML
-    public void initialize() {
-        ToggleGroup group = new ToggleGroup();
-        radioPix.setToggleGroup(group);
-        radioDoc.setToggleGroup(group);
-        radioTed.setToggleGroup(group);
-
-        transacao.setIdContaOrigem(ContaLogadaController.getInstance().getConta().getIdConta());
-    }
-
-    public void setIdContaOrigem(int idContaOrigem){
+    public void setIdContaOrigem(int idContaOrigem) {
         transacao.setIdContaOrigem(idContaOrigem);
     }
 
-    private void setTipoTransacao() {
-        if (!radioPix.isSelected() && !radioDoc.isSelected() && !radioTed.isSelected()) {
-            Alerta.exibirAlertaErro("Erro na transação", "Selecione um meio de pagamento.");
-            return;
-        }
-        if (radioPix.isSelected()) {
-            transacao.setIdTipoTransacao(1);
-        } else if (radioDoc.isSelected()) {
-            transacao.setIdTipoTransacao(2);
-        } else if (radioTed.isSelected()) {
-            transacao.setIdTipoTransacao(3);
-        }
+    @FXML
+    private void navegarParaTransacaoPix() throws IOException {
+        transacao.setIdTipoTransacao(1); // PIX
+        abrirTelaTransacao();
     }
 
-    public void navegarContaTransacao(ActionEvent actionEvent) throws IOException {
-        setTipoTransacao();
-        if (transacao.getIdTipoTransacao() != 0) {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/agibank/corehub/views/contaTransacao.fxml"));
-            Parent root = loader.load();
-
-            ContaTransacaoController contaTransacaoController = loader.getController();
-            contaTransacaoController.transacao = transacao;
-
-            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root, 412, 915);
-            stage.setScene(scene);
-            stage.show();
-        }
+    @FXML
+    private void navegarParaTransacaoDoc() throws IOException {
+        transacao.setIdTipoTransacao(2); // DOC
+        abrirTelaTransacao();
     }
 
-    public void navegarConta(ActionEvent actionEvent) throws IOException, SQLException {
+    @FXML
+    private void navegarParaTransacaoTed() throws IOException {
+        transacao.setIdTipoTransacao(3); // TED
+        abrirTelaTransacao();
+    }
+
+    private void abrirTelaTransacao() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/agibank/corehub/views/contaTransacao.fxml"));
+        Parent root = loader.load();
+
+        ContaTransacaoController contaTransacaoController = loader.getController();
+        contaTransacaoController.transacao = transacao;
+
+        Stage stage = (Stage) pixButton.getScene().getWindow(); // qualquer StackPane serve
+        Scene scene = new Scene(root, 412, 915);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void navegarConta() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/agibank/corehub/views/conta.fxml"));
         Parent root = loader.load();
 
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Stage stage = (Stage) pixButton.getScene().getWindow();
         Scene scene = new Scene(root, 412, 915);
         stage.setScene(scene);
         stage.show();
