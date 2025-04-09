adb412fbeaaf36639a7a938c614c8b45ce7ca59a
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-09 20:02:42 -0300

    :lipstick: fix(cadastro, tipoTransacao) ajusta interface melhorando design

--- src/main/java/com/agibank/corehub/App.java
+++ src/main/java/com/agibank/corehub/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/agibank/corehub/views/cadastro.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/agibank/corehub/views/telainicial.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 412, 800);
         stage.setTitle("CoreHub");
         stage.setScene(scene);
