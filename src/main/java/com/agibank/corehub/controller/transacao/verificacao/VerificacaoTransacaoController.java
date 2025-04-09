2d39377fcd7d0ecd68a0860c7f616b3f78506bd5
heitor.delvecchi <heitordelveccho@hotmail.com>
2025-04-09 12:15:07 -0300

    :sparkles: Atualização VerificacaoTransacaoController

--- src/main/java/com/agibank/corehub/controller/transacao/verificacao/VerificacaoTransacaoController.java
+++ src/main/java/com/agibank/corehub/controller/transacao/verificacao/VerificacaoTransacaoController.java
@@ -1,15 +1,16 @@
 package com.agibank.corehub.controller.transacao.verificacao;
 
+import com.agibank.corehub.beans.conta.Conta;
 import com.agibank.corehub.beans.transacao.Transacao;
 
 import java.sql.SQLException;
 
 public class VerificacaoTransacaoController {
-    public void verificarTransacao(Transacao transacao) throws SQLException {
+    public void verificarTransacao(Conta conta, Transacao transacao) throws SQLException {
         IntegridadeTransacaoController integridadeTransacaoController = new IntegridadeTransacaoController();
         SegurancaTransacaoController segurancaTransacaoController = new SegurancaTransacaoController();
 
-        integridadeTransacaoController.verificarIntegridadeTransacaoParaCadastrarEmStatus(transacao);
+        integridadeTransacaoController.verificarIntegridadeTransacaoParaCadastrarEmStatus(conta, transacao);
         segurancaTransacaoController.verificarSegurancaTransacao(transacao);
     }
 }
